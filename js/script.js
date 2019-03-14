0e8f64ee86633f8f72de6f2f941f92f6b3417463
Ignacio Cerna <ignaciocerna@ccpaedu.com>
2019-03-14 00:51:22 +0000

    Did some changes

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
-
 $("#search-button").click(function(){
-  
-  
-  
+    var searchTerm = ("search-term").val();
+  $.ajax({
+  url: "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&rating=pg&api_key=dc6zaTOxFJmzC",
+method: "GET",
+  success: function(response) {
+      var url = response.data[0].images.original.url;
+      alert(url);
+ $('body').append('<img src="' + url + '">');
+  },
+});
 });
 
